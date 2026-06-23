fedef4e873d7a2f185d5855ce85f8bec4c355650
Kwame Owusu <98961379+kwame-Owusu@users.noreply.github.com>
2026-06-23 18:10:10 +0100

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortArray(self, arr: List[int]) -> List[int]:
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+
+            # Move elements of arr[0..i-1] that are greater than key
+            # to one position ahead of their current position
+            while j >= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]
+                j -= 1
+
+            # Insert the key at its correct position
+            arr[j + 1] = key
+        
+        return arr
