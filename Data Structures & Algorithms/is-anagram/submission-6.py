6d75c5d45b28543460808e4d45998539dfbefb28
Kwame Owusu <98961379+kwame-Owusu@users.noreply.github.com>
2026-07-09 19:20:58 +0100

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count_s = defaultdict(int)
+        count_t = defaultdict(int)
+
+        for char in s:
+            count_s[char] += 1
+        
+        for char in t:
+            count_t[char] += 1
+        
+
+        for key, val in count_s.items():
+            if key not in count_t:
+                return False
+            
+            if val != count_t[key]:
+                return False
+            
+
+        return True
+        
+
+        
+        
+        
