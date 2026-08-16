ba762504381fa94a9b810014327256ceb7f8adb8
Kwame Owusu <98961379+kwame-Owusu@users.noreply.github.com>
2026-08-16 18:56:19 +0100

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = ""
+        for char in s:
+            if char.isalnum():
+                cleaned += char.lower()
+        
+        l, r = 0, len(cleaned) - 1
+
+        while l < r:
+            if cleaned[l] != cleaned[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+        
+        
